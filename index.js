9e5f7c948b39630924680cb2826f0250af114892
giovo_adv@hotmail.com <giovo_adv@hotmail.com>
2024-06-24 14:28:49 -0500

    created pokecard for each pokemon

--- index.js
+++ index.js
@@ -16,9 +16,31 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
     const res = await fetch(url);
     const pokemonData = await res.json();
-    console.log(pokemonData)
+    console.log(pokemonData);
+    createPokemonCard(pokemonData);
   } catch (error) {
     console.log(error);
   }
 };
-fetchPokemons()
+
+const createPokemonCard = (pokemonData) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const pokemonInnerHtml = `<div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png"
+            alt=""
+          />
+        </div>
+        <div class="info">
+          <span class="number">${pokemonData.id}</span>
+          <h3 class="name">${pokemonData.name}</h3>
+          <small class="type">Type <span>${pokemonData.types[0].type.name}</span></small>
+        </div>`;
+  pokemonEl.innerHTML = pokemonInnerHtml;
+  console.log(pokemonEl);
+  pokeContainer.appendChild(pokemonEl);
+};
+
+fetchPokemons();
